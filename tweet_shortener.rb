914d0ff386c3a9bd15b148b902fd012685db3bc3
Craig Donavin <crdonavin@gmail.com>
2019-04-11 00:24:27 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 def dictionary
-words = {"hello" => "hi"
-"to", "two", "too" => "2"
-"for", "four" => "4"
-"be" => "b"
-"you" => "u"
-"at" => "2"
+words = {"hello" => "hi",
+"to", "two", "too" => "2",
+"for", "four" => "4",
+"be" => "b",
+"you" => "u",
+"at" => "2",
 "and" => "&"}
 end
