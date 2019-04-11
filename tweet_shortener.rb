07837fe6c3a1a41f8929adeb1c9163d1b7c255dd
Craig Donavin <crdonavin@gmail.com>
2019-04-11 00:45:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,3 @@
-require pry
 # Write your code here.
 def dictionary
 words = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4",
@@ -7,5 +6,5 @@ end
 
 def word_substituter(string)
   string.split(/ /)
-  binding.pry
+  dictionary.keys
 end
