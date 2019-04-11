125dcf3e16ff24982244e1d5b4b261d73b049915
Craig Donavin <crdonavin@gmail.com>
2019-04-11 01:41:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,19 +5,5 @@ words = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4",
 end
 
 def word_substituter(string)
-  counter = 0
-  tweet_ary = string.split(/ /)
-  tweet_ary.each do |word|
-    while counter<tweet_ary.length
-      if dictionary.keys.include?(word)
-        dictionary.each do |key, subst|
-          if key == word
-            tweet_ary[counter] = subst
-          end
-        end
-        counter+=1
-      end
-    end
-  end
-tweet_ary.join(" ")
+dictionary.values
 end
