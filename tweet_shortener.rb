338d75d63a649018e718ad2a733900b449e08afe
Craig Donavin <crdonavin@gmail.com>
2019-04-11 00:37:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,3 +3,7 @@ def dictionary
 words = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4",
 "be" => "b", "you" => "u", "at" => "2", "and" => "&"}
 end
+
+def word_substituter(string)
+  string.split(/ /)
+end
