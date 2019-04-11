f90788c5949c356d36fa282e20d19573aab9dca0
Craig Donavin <crdonavin@gmail.com>
2019-04-11 03:33:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 def dictionary
-words = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4",
+words = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2","For" => "4", "for" => "4", "four" => "4",
 "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
 end
 
 def word_substituter(string)
 
   string.split.collect do |word|
-    if dictionary.keys.include?(word.downcase)
+    if dictionary.keys.include?(word)
       word = dictionary[word]
     else
       word
